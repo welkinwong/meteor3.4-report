4bc4cbac62c06589a449f313c7eb0a644ed01f0e
Welkin Wong <welkin.wong@me.com>
2025-11-14 12:22:22 +0800

    report rspack config issue

--- rspack.config.js
+++ rspack.config.js
@@ -10,6 +10,47 @@ const { defineConfig } = require('@meteorjs/rspack');
  *
  * Use these flags to adjust your build settings based on environment.
  */
-module.exports = defineConfig(Meteor => {
-  return {};
+module.exports = defineConfig((Meteor) => {
+  return {
+    // [Rspack Client Error] node:internal/modules/esm/resolve:314
+    //   return new ERR_PACKAGE_PATH_NOT_EXPORTED(
+    //          ^
+    // Error [ERR_PACKAGE_PATH_NOT_EXPORTED]: Package subpath './package.json' is not defined by "exports" in /Users/welkin/code/meteor3.4-report/node_modules/@dr.pogodin/react-helmet/package.json
+    //     at exportsNotFound (node:internal/modules/esm/resolve:314:10)
+    //     at packageExportsResolve (node:internal/modules/esm/resolve:661:9)
+    //     at resolveExports (node:internal/modules/cjs/loader:661:36)
+    //     at Function._findPath (node:internal/modules/cjs/loader:753:31)
+    //     at Function._resolveFilename (node:internal/modules/cjs/loader:1391:27)
+    //     at Function.resolve (node:internal/modules/helpers:145:19)
+    //     at pkgDir (/Users/welkin/code/meteor3.4-report/node_modules/@meteorjs/rspack/lib/meteorRspackHelpers.js:11:28)
+    //     at Array.map (<anonymous>)
+    //     at compileWithRspack (/Users/welkin/code/meteor3.4-report/node_modules/@meteorjs/rspack/lib/meteorRspackHelpers.js:41:51)
+    //     at Meteor.compileWithRspack (/Users/welkin/code/meteor3.4-report/node_modules/@meteorjs/rspack/rspack.config.js:268:5) {
+    //   code: 'ERR_PACKAGE_PATH_NOT_EXPORTED'
+    // }
+    ...Meteor.compileWithRspack(['@dr.pogodin/react-helmet']),
+
+    // Directly using the include configuration in Rspack works normally.
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.(?:[mc]?js|jsx|[mc]?ts|tsx)$/i,
+    //       include: [
+    //         './node_modules/@dr.pogodin/react-helmet',
+    //       ],
+    //       loader: 'builtin:swc-loader',
+    //       options: {
+    //         jsc: {
+    //           baseUrl: './',
+    //           paths: { '/*': ['*', '/*'] },
+    //           parser: { syntax: 'typescript', tsx: true },
+    //           target: 'es5',
+    //           transform: { react: { development: false, refresh: false } },
+    //           externalHelpers: true,
+    //         },
+    //       },
+    //     },
+    //   ],
+    // },
+  };
 });
